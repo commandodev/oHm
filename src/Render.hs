29e1e595bb2232b6a784000269521133c96c35a9
Kris Jenkins <krisajenkins@gmail.com>
2014-11-26 15:22:23 +0000

    Minor CSS changes.

--- src/Render.hs
+++ src/Render.hs
@@ -8,10 +8,10 @@ rootView send state =
   vnode "div.container"
         [vnode "nav.navbar.navbar-default" [vtext "div.navbar-brand" "Demo"]
         ,vnode "div.row"
-               [vnode "div.col-md-3"
+               [vnode "div.col-sm-3"
                       [msgButton (IncFst 5) "+(5, 0)"
                       ,msgButton (IncSnd 3) "+(0, 3)"
                       ,msgButton (DecBoth 5) "-(10, 10)"]
-               ,vnode "div.col-md-9.well" [vtext "div" (show state)]]]
+               ,vnode "div.col-sm-9" [vtext "div.well" (show state)]]]
   where msgButton msg text =
           vbutton "button.btn.btn-primary" (\_ -> send msg) text
