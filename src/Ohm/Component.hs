a095d6a3994f7d3613ec28b7a48183d61ce4d331
Ben Ford <ben@perurbis.com>
2015-01-10 08:32:04 +0000

    A Add some haddocks for Component

--- src/Ohm/Component.hs
+++ src/Ohm/Component.hs
@@ -25,11 +25,18 @@ import MVC hiding (handles)
 import Ohm.HTML (Renderer, domChannel, newTopLevelContainer, renderTo)
 import Pipes ((~>))
 
+-- | Converts @UI Events@ to @Model Events@ in a monad for external communication
+--
+-- see "Pipes.Tutorial" for more on function @a -> 'Producer' b m ()@
 newtype Processor m edom ein = Processor
   { 
     runProcessor :: edom -> Producer ein m ()
   } deriving (Monoid)
 
+-- | Pass @UI Events@ directly to the model
+--
+-- Use this in the case of simple components that use the same type
+-- for both @UI Events@ and @Model Events@
 idProcessor :: (Monad m) => Processor m e e
 idProcessor = Processor yield
 
@@ -40,7 +47,7 @@ instance (Monad m) => Profunctor (Processor m) where
   lmap f (Processor p) = Processor (p . f)
   rmap = fmap
   
--- | Use a Prism to determine which part of a message to handle
+-- | Use a Prism' to determine which part of a message to handle
 handles :: Monad m => Prism' b a -> Processor m a c -> Processor m b c  
 handles prism p = Processor $ traverse_ (runProcessor p) . preview prism
 
@@ -49,18 +56,17 @@ handles prism p = Processor $ traverse_ (runProcessor p) . preview prism
 
 data Component env ein model edom = Component {
    
-    -- | The processing function of a 'Model' for this component
+    -- | The processing function of an 'MVC.Model' for this component
     model :: ein -> model -> model
     
-    -- | A renderer for 'Model' creating 'HTML' produces 'DOMEvent's
+    -- | A renderer for 'MVC.Model' creating 'HTML' produces @UI Events@
   , render :: Renderer edom model
   
     -- | A processor of events emitted from the UI
-    --
-    -- This has the choice of feeding events back into the model
   , domEventsProcessor :: Processor (ReaderT env IO) edom ein
   }
 
+-- | Convert's the 'model' function from a 'Component' to an 'MVC.Model'
 appModel :: (e -> m -> m) ->  Model m e m
 appModel fn = asPipe $ forever $ do
   e <- await
@@ -69,14 +75,27 @@ appModel fn = asPipe $ forever $ do
 
   
 --------------------------------------------------------------------------------
+{- | Runs a 'Component'
 
+Running a component forks two IO threads:
+
+ * The first is for @UI Events@. The thead processes them, running any
+   IO actions and feeding @Model Events@ into the 'MVC.Model' 'Controller'
+ 
+ * The second is the thread which calls 'MVC.runMVC' and updates the DOM
+-}
 runComponent'
-  ::  forall model env ein edom. (Show model)
-  => (model -> IO ())
+  ::  forall model env ein edom.
+     (model -> IO ())
+  -- ^ An action to run every time the model updates (used by 'runComponentDebug')
   -> model
+  -- ^ Initial model state
   -> env
+  -- ^ Configuration available in 'ReaderT'
   -> Component env ein model edom
+  -- ^ The component to run
   -> IO (Output ein)
+  -- ^ Returns an 'Output' that accepts @Model Events@ from external systems
 runComponent' dbg s env Component{..} = do
   (domSink, domSource) <- spawn Unbounded
   (modelSink, modelSource) <- spawn Unbounded
@@ -86,19 +105,25 @@ runComponent' dbg s env Component{..} = do
   void . forkIO . void $ runMVC s (appModel model) $ app modelSource domSink
   return modelSink
   where
+    -- Run the event processor over the events produced by the dom
     runEvents :: Effect (ReaderT env IO) () -> IO ()
     runEvents = void . forkIO . (flip runReaderT $ env) . runEffect
+    
+    -- Create an MVC app
     app :: Input ein -> Output edom -> Managed (View model, Controller ein)
     app eventSource domSink = managed $ \k -> do
       let render' = render (domChannel domSink)
       componentEl <- newTopLevelContainer
       renderTo componentEl $ render' s
       k (asSink (debugRender render' componentEl), asInput eventSource)
-    --debugRender :: ()
+
+    -- Render the current model by patching the DOM
+    -- debugRender :: (model -> HTML) -> VNodePresentation -> model -> IO ()
     debugRender f el mdl = do
       dbg mdl
       renderTo el $ f mdl
 
+-- | 'runComponent'' printing the model at each modification
 runComponentDebug
   ::  forall model env ein edom. (Show model)
   => model
@@ -107,9 +132,10 @@ runComponentDebug
   -> IO (Output ein)      
 runComponentDebug = runComponent' print
 
+-- | 'runComponent'' with no extra action
 runComponent
-  ::  forall model env ein edom. (Show model)
-  => model
+  ::  forall model env ein edom.
+     model
   -> env
   -> Component env ein model edom
   -> IO (Output ein)      
