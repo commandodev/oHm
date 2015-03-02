5304abc7bd3c6b87fb0af1e988992e9c51a84641
Ben Ford <ben@perurbis.com>
2015-03-02 20:06:14 +0000

    Add on* callbacks and preventDefaut

--- src/Ohm/HTML.hs
+++ src/Ohm/HTML.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Ohm.HTML
   ( -- * bootstrap operators
@@ -10,6 +11,12 @@ module Ohm.HTML
     -- * Utilities
   , mkButton
   , embedRenderer
+  
+    -- * Callbacks
+  , onClick
+  , onChange
+  , onKeyPress
+  , onInput
     
     -- * Types
   , Renderer
@@ -18,10 +25,19 @@ module Ohm.HTML
   , module Control.Applicative
   , module VirtualDom
   , module Ohm.DOMEvent
+  , module GHCJS.Types
   , lmap, dimap
   ) where
 import Control.Lens hiding (aside, children, coerce, pre)
+import Control.Monad.State
+import Data.Foldable
 import GHCJS.Foreign
+import GHCJS.Marshal
+import GHCJS.Types
+import GHCJS.DOM.Event
+import GHCJS.DOM.HTMLInputElement
+import GHCJS.DOM.Types (GObject, toGObject, unsafeCastGObject)
+import GHCJS.DOM.UIEvent
 import VirtualDom
 import VirtualDom.Prim
 import Ohm.DOMEvent
@@ -43,7 +59,7 @@ col3 = bsCol 3
 col6 = bsCol 6
 col9 = bsCol 9
 
-mkButton :: IO () -> HTML -> [String] -> HTML
+mkButton :: (JSRef Event -> IO ()) -> HTML -> [String] -> HTML
 mkButton msgAction btnTxt classList = 
   with button_
     (do
@@ -60,3 +76,35 @@ embedRenderer subRenderer prsm l evt mdl = subRenderer converted focussed
 classes :: Traversal' HTMLElement [String]
 classes = attributes . at "class" . anon "" (isEmptyStr . fromJSString) . iso (words . fromJSString) (toJSString . unwords)
   where isEmptyStr = (== ("" :: String))
+
+
+preventDefault :: JSRef Event -> IO ()
+preventDefault evt = traverse_ eventPreventDefault =<< fromJSRef evt
+
+onInput :: MonadState HTMLElement m => DOMEvent String -> m ()
+onInput chan = on "input" f
+  where
+  f evt = do
+    t <- fromJSRef evt
+    for_ t (\t' -> do
+      t'' <- eventGetTarget t'
+      for_ t''
+        (htmlInputElementGetValue .
+         castToHTMLInputElement >=> (channel chan)))
+
+onClick :: MonadState HTMLElement m => DOMEvent () -> m ()
+onClick chan = on "click" $ (void . preventDefault >=> channel chan)
+
+onChange :: MonadState HTMLElement m => DOMEvent () -> m ()
+onChange chan = on "change" $ (void . preventDefault >=> channel chan)
+
+onKeyPress :: MonadState HTMLElement m => DOMEvent Int -> m ()
+onKeyPress (DOMEvent chan) = on "keypress" f
+  where
+  f evt = do
+    t <- fromJSRef evt
+    for_ t
+         (uiEventGetKeyCode .
+          -- A little messy, but we're working with a dom-delegator 'KeyEvent' here.
+          (unsafeCastGObject :: GObject -> UIEvent) .
+           toGObject >=> chan)
